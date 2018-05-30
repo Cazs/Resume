3befc4b411ddcac39b89f2373e34431296326df4
Casper Ndlovu <casperndlovu42@gmail.com>
2018-05-30 11:20:00 +0200

    Fixed Skill component positioning.

--- src/components/skills/Skill.jsx
+++ src/components/skills/Skill.jsx
@@ -19,13 +19,13 @@ class Skill extends React.Component
                                 <p style={{margin: '0px 10px 0px 0px', textAlign: 'left', float: 'left'}}>{skill.title}</p>
                             </div>
                             <div className='col-lg-4'>
-                                <div style={{display: 'flex', width: '50%', height: '5px', backgroundColor: '#3c3c3c', borderRadius: '5px', marginTop: '-15px', marginLeft: '-50px', float: 'right'}}>
+                                <div className='skill-bar'>
                                     <div style={{width: `${skill.level}%`, height: '5px', backgroundColor: '#ff7400', borderRadius: '5px'}} />
-                                    <div style={{width: '20px', height: '20px', backgroundColor: '#ff7400', borderRadius: '20px', margin: '-7px 0px 0px -3px'}} />
+                                    <div style={{width: '20px', height: '20px', backgroundColor: '#ff7400', borderRadius: '20px', margin: `-13px 0px 0px ${skill.level-5}%`}} />
                                 </div>
                             </div>
                             <div className='col-lg-4'>
-                                <p style={{margin: '-25px -45px 0px 0px', textAlign: 'right', float: 'right'}}> {skill.level}%</p>
+                                <p className='skill-level' style={{textAlign: 'right', float: 'right'}}> {skill.level}%</p>
                             </div>
                         </div>
                     )
