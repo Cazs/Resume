eca2767f3bda68c5ab19ed0d1b8f7918793756aa
Casper Ndlovu <casperndlovu42@gmail.com>
2023-06-09 00:32:33 +0200

    fix: added new videos

--- src/components/petprojects/index.jsx
+++ src/components/petprojects/index.jsx
@@ -59,65 +59,88 @@ class PetProjects extends React.Component {
                 <div id="accordion">
                     <div className="card">
                         <div className="card-header">
-                            <a className="card-link" data-toggle="collapse" href="#collapseOne">
+                            <a className="card-link" data-toggle="collapse" href="#collapse1">
                                 Capture The Flag Game (3rd Year Computer Science Project)
                             </a>
                         </div>
-                        <div id="collapseOne" className="collapse show" data-parent="#accordion">
+                        <div id="collapse1" className="collapse show" data-parent="#accordion">
                             <div className="card-body">
+                                <img className="pet-project-preview" src="https://image.ibb.co/iHVTMT/ctf600x375_15fps.gif" style={{ marginBottom: '15px' }} width="240" height="180" alt="Capture The Flag game preview" datatype="gif" />
+                                <br/>
                                 <a href='https://calm-ground-0c6108310.3.azurestaticapps.net/static/media/capture-the-flag.zip'>
                                     <span role="img" aria-label="Floppy Disk Emoji">💾 Download Capture The Flag</span>
                                 </a>
                             </div>
                         </div>
                     </div>
+                    
                     <div className="card">
                         <div className="card-header">
-                            <a className="card-link" data-toggle="collapse" href="#collapseTwo">
+                            <a className="card-link" data-toggle="collapse" href="#collapse2">
                                 Platformer Game Level Editor (2nd Year Computer Science Project)
                             </a>
                         </div>
-                        <div id="collapseTwo" className="collapse show" data-parent="#accordion">
+                        <div id="collapse2" className="collapse" data-parent="#accordion">
                             <div className="card-body">
+                                <img className="pet-project-preview"  src="https://i.ibb.co/D9m4t93/level-editor700x438-30fps.gif" style={{ marginBottom: '15px' }} width="240" height="180" alt="Capture The Flag game preview" datatype="gif" />
+                                <br/>
                                 <a href='https://calm-ground-0c6108310.3.azurestaticapps.net/static/media/platformer-level-editor.zip'>
                                     <span role="img" aria-label="Floppy Disk Emoji">💾 Download Platformer Level Editor</span>
                                 </a>
                             </div>
                         </div>
                     </div>
+                    
                     <div className="card">
                         <div className="card-header">
-                            <a className="card-link" data-toggle="collapse" href="#collapseThree">
+                            <a className="card-link" data-toggle="collapse" href="#collapse3">
                                 Arduino Mega Stuff
                             </a>
                         </div>
-                        <div id="collapseThree" className="collapse show" data-parent="#accordion">
+                        <div id="collapse3" className="collapse" data-parent="#accordion">
                             <div className="card-body">
                                 <iframe className="pet-project-video" src="https://drive.google.com/file/d/1x2RGKhvKIErYOzhJkaq4uxwI_6WfTQFp/preview" allow="fullscreen"></iframe>
+                                <br />
+                                <iframe className="pet-project-video" src="https://drive.google.com/file/d/1T5vddSPC97gu36LvCkJgvx4eu74zPr9e/preview" allow="fullscreen"></iframe>
                             </div>
                         </div>
                     </div>
+                    
                     <div className="card">
                         <div className="card-header">
-                            <a className="collapsed card-link" data-toggle="collapse" href="#collapseFour">
+                            <a className="collapsed card-link" data-toggle="collapse" href="#collapse4">
                                 RC Car Project
                             </a>
                         </div>
-                        <div id="collapseFour" className="collapse" data-parent="#accordion">
+                        <div id="collapse4" className="collapse" data-parent="#accordion">
                             <div className="card-body">
                                 <iframe className="pet-project-video" src="https://drive.google.com/file/d/1uEUQSd3nP1A1ky5FIsFp18r6Y7qM5xS0/preview" allow="fullscreen"></iframe>
                             </div>
                         </div>
                     </div>
+                    
                     <div className="card">
                         <div className="card-header">
-                            <a className="collapsed card-link" data-toggle="collapse" href="#collapseFive">
+                            <a className="collapsed card-link" data-toggle="collapse" href="#collapse5">
                                 Makeshift Steering System for RC car
                             </a>
                         </div>
-                        <div id="collapseFive" className="collapse" data-parent="#accordion">
+                        <div id="collapse5" className="collapse" data-parent="#accordion">
                             <div className="card-body">
                                 <iframe className="pet-project-video" src="https://drive.google.com/file/d/1LmSRjo13e7qKwGtfdDrCESt_LZ6EDkj7/preview" allow="fullscreen"></iframe>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div className="card">
+                        <div className="card-header">
+                            <a className="collapsed card-link" data-toggle="collapse" href="#collapse6">
+                                Makeshift dashcam using a generic IP cam & 9v battery
+                            </a>
+                        </div>
+                        <div id="collapse6" className="collapse" data-parent="#accordion">
+                            <div className="card-body">
+                                <iframe className="pet-project-video" src="https://drive.google.com/file/d/1Xw3gJk5i_csiskaKGK4pw-G3UHdwl3UY/preview" allow="fullscreen"></iframe>
                             </div>
                         </div>
                     </div>
